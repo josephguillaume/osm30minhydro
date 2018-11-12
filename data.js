8db72527f779ec8eb197c08e4b14ce9b97fe4cdd
Joseph Guillaume <josephguillaume@gmail.com>
2018-11-12 15:32:51 +0200

    Show ddm30 lines for selected basin

--- data.js
+++ data.js
@@ -28,6 +28,49 @@ async function load_ddm30_basins(mapLayers, select_basin) {
   );
 }
 
+async function fetch_ddm30_lines(mapLayers) {
+  const response = await fetch("data/ddm_lines_cells.json");
+  const ddm30_lines = await response.json();
+
+  mapLayers.data.ddm_lines_cells = await ddm30_lines;
+}
+
+load_basin_ddm = function(mapLayers, basin_id) {
+  mapLayers.remove("basin_ddm");
+
+  const ddmLinesStyle = {
+    color: "black",
+    weight: 2,
+    opacity: 0.65,
+    clickable: false
+  };
+
+  layer_ddmLines_cells_line = L.geoJson(mapLayers.data.ddm_lines_cells, {
+    style: ddmLinesStyle,
+    filter: (feature, layer) => feature.properties.basin == basin_id
+  });
+  layer_ddmLines_cells_arrowhead = L.polylineDecorator(
+    layer_ddmLines_cells_line.getLayers(),
+    {
+      patterns: [
+        {
+          offset: "100%",
+          repeat: 0,
+          symbol: L.Symbol.arrowHead({
+            pixelSize: 10,
+            pathOptions: { fillOpacity: 1, weight: 0, color: "black" }
+          })
+        }
+      ]
+    }
+  );
+  layer_ddmLines_cells = L.layerGroup([
+    layer_ddmLines_cells_line,
+    layer_ddmLines_cells_arrowhead
+  ]);
+  mapLayers.add(layer_ddmLines_cells, "basin_ddm");
+};
+
 async function get_basin_wiki(object = window) {
   const response = await fetch("basin_wiki.csv");
   const text = await response.text();
